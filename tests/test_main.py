bb623fb343c6026cfd664cac61ad3b3014be9983
manishramamoorthy <manishram2019@gmail.com>
2025-11-27 16:30:16 +0530

    Update a sonar code

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello from FastAPI - Sonar Test Project"}
+
+def test_add_numbers():
+    response = client.get("/add/2/3")
+    assert response.status_code == 200
+    assert response.json()["result"] == 5
